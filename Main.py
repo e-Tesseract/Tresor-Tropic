a3bf0d94234c4beb31a909de4e014bac6925a3bc
CAPON Hugo <hugo.capon@etu.univ-littoral.fr>
2023-10-06 10:47:57 +0200

    modif du main pour se deplacer en fonction des cases et nom en fonction du numéro du dés

--- Main.py
+++ Main.py
@@ -15,7 +15,7 @@ for i in range(1, nombre_de_joueurs + 1):
 plateau = Plateau(joueurs=joueurs)
 
 print("---------------------- MAIN ----------------------\n")
-while all(joueur.position < 31 for joueur in joueurs):
+while all(joueur.position < 30 for joueur in joueurs):
     # Tant que les joueurs ne sont pas sur la case 31, continuez le jeu
     print("---------------------- TOUR ----------------------")
 
@@ -28,16 +28,37 @@ while all(joueur.position < 31 for joueur in joueurs):
 
     for joueur in joueurs:
         print(f"\nTour de {joueur.nom}")
-        deplacement = joueur.choix_deplacement()
+
+        # ------------------------------------------------------------------- #
+        reultat_lancer_des = joueur.lancer_de_des()                                                         
+        print("Résultat du lancer de dés du Joueur: ", reultat_lancer_des)
+        cases_disponible = []
+
+        for i in range(1, reultat_lancer_des + 1):
+            cases_disponible.append(i + joueur.position)
+        
+        print("Cases disponible: ", cases_disponible)
+
+        while True:
+            choix_case = int(input("Choisissez un déplacement: "))
+            if int(choix_case) in cases_disponible and int(choix_case) != 0:
+                break
+            else:
+                print("Choix invalide. Veuillez choisir un déplacement valide.")
+
+        deplacement = choix_case
+
+        # ------------------------------------------------------------------- #
+
         ancienne_position = joueur.position
 
-        for i in range(ancienne_position, ancienne_position + deplacement):
+        for i in range(ancienne_position, deplacement):
             print(plateau.cases[i]["description"])
             if plateau.cases[i]["description"] == "Monstre":
                 if plateau.combat_monstre(joueur) is False:
                     print("Case du monstre: ", plateau.cases[i]["numero"])
                     print("Case à reculer: ", plateau.cases[i]["numero"] - 1)
-                    deplacement = i - ancienne_position
+                    deplacement = plateau.cases[i]["numero"] - 1
                     break
 
 
@@ -46,11 +67,21 @@ while all(joueur.position < 31 for joueur in joueurs):
         for i in range(len(joueurs)):
             for j in range(i + 1, len(joueurs)):
                 if joueurs[i].position == joueurs[j].position:
-                    if joueur.position == 1:
-                        continue
-                    gagnant = plateau.combat_joueurs(joueurs[i], joueurs[j])
-                    perdant = joueurs[j] if gagnant == joueurs[i] else joueurs[i] 
-                    plateau.deplacer_joueur(perdant, perdant.position, -1)
+                    if joueur.position != 1:       
+                        gagnant = plateau.combat_joueurs(joueurs[i], joueurs[j])
+                        perdant = joueurs[j] if gagnant == joueurs[i] else joueurs[i] 
+                        # tant que le perdant tombe sur une case ou un autre joueur est déjà présent, le perdant est déplacé sur la case précédente
+                        nouvelle_position_perdant = perdant.position - 1  # Nouvelle position après avoir perdu
+                        while nouvelle_position_perdant >= 1 and plateau.cases[nouvelle_position_perdant - 1]["joueurs_sur_case"]:
+                            # dire quel joueur est déjà présent sur la case si le joeur ne dessent pas en dessous de 1
+                            
+                            print(f"Le joueur {plateau.cases[nouvelle_position_perdant - 1]['joueurs_sur_case'][0].nom} est déjà présent sur la case {nouvelle_position_perdant}. Le joueur {perdant.nom} est tombe sur la case {nouvelle_position_perdant - 1}.")
+                            nouvelle_position_perdant -= 1
+                        # Vérifie si la nouvelle position est inférieure à 1
+                        if nouvelle_position_perdant < 1:
+                            nouvelle_position_perdant = 1
+                        plateau.deplacer_joueur(perdant, perdant.position, nouvelle_position_perdant)
+                        
             for joueur in joueurs:
                 plateau.mettre_a_jour_joueurs_sur_case(joueur, joueur.position)
 
